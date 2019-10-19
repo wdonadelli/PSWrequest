4584b218ba0d1197cc776dd07208a8e44a8fdd74
wdonadelli <wdonadelli@gmail.com>
2019-10-19 01:17:26 -0300

    testando

--- PSWrequest.php
+++ PSWrequest.php
@@ -1,70 +1,140 @@
 ﻿<?php
 setlocale (LC_ALL, "pt_BR");
 
-class DB extends SQLite3 {
+//ini_set('display_errors',1);
+//ini_set('display_startup_erros',1);
+error_reporting(E_ALL);
 
-	public function __construct($db = NULL) {
-		/*Instanciar o objeto: arquivo obrigatório*/
-		if (!is_file($db) && $db !== ":memory:") {
-			throw new Exception("DB: wrong argument");
+sfsdfsdf
+echo("bunda");
+
+
+
+class PSWrequest extends SQLite3 {
+
+	/*-- erros: constantes --*/
+	private $DB    = "construct: Undefined database!";
+	private $TABLE = "\$table: Enter the table name accordingly!";
+	private $DATA  = "\$data: The input value must be an array with content!";
+	private $WHERE = "\$where: The input value must be an array with content!";
+	private $BOOL  = "\$bool: The input value must be boolean!";
+
+	/*-- erros: verificação --*/
+	private function checkError($DB = NULL, $table = NULL, $data = NULL, $wherer = NULL, $bool = NULL) {
+		if ($DB    !== NULL && (!is_file($DB) && $DB !== ":memory:")) {
+			throw new Exception($CONSTRUCTOR);
 		}
-		if (is_file($db)) {
-			//chmod(dirname($db), 0777);
+		if ($table !== NULL && (gettype($table) !== "string" || strlen(trim($table)) == 0)) {
+			throw new Exception($TABLE);
 		}
-		$this->open($db);
+		if ($data  !== NULL && (gettype($data) !== "array" || count($data) == 0)) {
+			throw new Exception($DATA);
+		}
+		if ($where !== NULL && (gettype($where) !== "array" || count($where) == 0)) {
+			throw new Exception($WHERE);
+		}
+		if ($bool !== NULL && (gettype($where) !== "boolean") {
+			throw new Exception($BOOL);
+		}
+		return 0;
+	}
+
+	/*-- Construtor: define o banco de dados --*/
+	public function __construct($DB = NULL) {
+		$this->checkError($DB);
+		$this->open($DB);
 		return;
 	}
 
-	public function go($sql) {
-		if (strlen(trim($sql)) === 0) {
-			throw new Exception("DB->go: wrong argument");
-		}
-		
-		$query = preg_match('/^SELECT/i', trim($sql)) ? True : False;
-		$exec  = $query ? $this->query($sql) : $this->exec($sql);
-
+	/*-- output: imprime o retorno da requisição --*/
+	private function output($query, $exec) {
+		$data = [];
 		if (!$query || $this->lastErrorCode() !== 0) {
-			$data = [
-				"error" => $this->lastErrorCode() !== 0 ? True : False,
-				"msg"   => $this->lastErrorMsg(),
-				"id"    => $this->lastInsertRowID()
-			];
+			$data["error"]   = $this->lastErrorCode() !== 0 ? True : False;
+			$data["message"] = $this->lastErrorMsg();
+			$data["id"]      = $this->lastInsertRowID();
 		} else {
-			$data = [];
 			while ($i = $exec->fetchArray()) {
 				array_push($data, $i);
 			}
 		}
-
 		echo json_encode($data);
+		return 0;
+	}
 
-		return;
+	/*-- sql: define o que fazer a partir de um comando sql --*/
+	public function sql($input) {
+		$sql   = trim($input);
+		$query = preg_match('/^SELECT/i', $sql) ? True : False;
+		$exec  = $query ? $this->query($sql) : $this->exec($sql);
+		$this->output($query, $exec);
+		return 0;
 	}
 	
-	public function view($table, $where = NULL) {
-		$this->go("SELECT * FROM ".$table.($where === NULL ? ";" : " WHERE ".$where.";"));
-		return;
-	}
-	
-	public function insert($table, $array) {
-		if (count($array) === 0) {
-			throw new Exception("DB->insert: Empty array");
+	/*-- insert: insere dados na tabela a partir de um array --*/
+	public function insert($table, $data) {
+		$this->checkError(NULL, $table, $data);
+		$col = [];
+		$val = [];
+		foreach($data as $key => $value) {
+			array_push($col, $key);
+			array_push($val, "'{$value}'");
 		}
-		$values  = "";
-		$columns = "";
-		foreach($array as $key => $value) {
-			$values  .= $values  === "" ? "'".$value."'" : ", '".$value."'";
-			$columns .= $columns === "" ? $key : ", ".$key;
-		}
-		$this->go("INSERT INTO ".$table." (".$columns.") VALUES (".$values.");");
-		return;
+		$col = join(", ", $col);
+		$val = join(", ", $val);
+		$sql = "INSERT INTO {$table} ({$col}) VALUES ({$val});";
+		$this->sql($sql);
+		return 0;
 	}
-	
-	public function vaccum() {
-		$this->go("VACUUM;");
-		return;
+
+	/*-- update: atualiza dados da tabela a partir de um array --*/
+	public function update($table, $data, $where, $and = True) {
+		$this->checkError(NULL, $table, $data, $where, $and);
+		$set = [];
+		$whr = [];
+		foreach($data as $key => $value) {
+			array_push($set, "{$key} = '{$value}'");
+		}
+		foreach($where as $key => $value) {
+			array_push($whr, "{$key} = '{$value}'");
+		}
+		$set = join(", ", $set);
+		$whr = join(($and == True ? " AND " : " OR "), $whr);
+		$sql = "UPDATE {$table} SET {$set} WHERE {$whr};";
+		$this->sql($sql);
+		return 0;
+	}
+
+	/*-- delete: exclui dados da tabela a partir de um array --*/
+	public function update($table, $where, $and = True) {
+		$this->checkError(NULL, $table, NULL, $where, $and);
+		$whr = [];
+		foreach($where as $key => $value) {
+			array_push($whr, "{$key} = '{$value}'");
+		}
+		$whr = join(($and == True ? " AND " : " OR "), $whr);
+		$sql = "DELETE FROM {$table} WHERE {$whr};";
+		$this->sql($sql);
+		return 0;
+	}
+
+	/*-- view: returna todos os itens da pesquisa --*/
+	public function view($table, $where, $and = True) {
+		$this->checkError(NULL, $table, NULL, NULL, $and);
+		if (gettype($where) == "array" && count($where) > 0) {
+			$whr = [];
+			foreach($where as $key => $value) {
+				array_push($whr, "{$key} = '{$value}'");
+			}
+			$whr = join(($and == True ? " AND " : " OR "), $whr);
+			$whr = " WHERE {$whr}"
+		} else {
+			$whr = "";
+		}
+		$sql = "SELECT * FROM {$table}{$whr};";
+		$this->sql($sql);
+		return 0;
 	}
 
 };
-
 ?>
