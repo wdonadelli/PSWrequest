91a9b5fc4911bfa34af8de8c4a6f456105cb24d5
wdonadelli <wdonadelli@gmail.com>
2019-10-20 01:40:43 -0300

    testando

--- PSWrequest.php
+++ PSWrequest.php
@@ -1,49 +1,99 @@
 ﻿<?php
+/*-- configuração --*/
+//phpinfo();
 setlocale (LC_ALL, "pt_BR");
-
-//ini_set('display_errors',1);
-//ini_set('display_startup_erros',1);
-error_reporting(E_ALL);
-
-sfsdfsdf
-echo("bunda");
-
-
+ini_set('display_errors', True);
 
 class PSWrequest extends SQLite3 {
 
-	/*-- erros: constantes --*/
-	private $DB    = "construct: Undefined database!";
-	private $TABLE = "\$table: Enter the table name accordingly!";
-	private $DATA  = "\$data: The input value must be an array with content!";
-	private $WHERE = "\$where: The input value must be an array with content!";
-	private $BOOL  = "\$bool: The input value must be boolean!";
+	/*-- PRIVADOS --*/
+	
+	/*-- resultado --*/
+	private $DATA = "[]";
 
-	/*-- erros: verificação --*/
-	private function checkError($DB = NULL, $table = NULL, $data = NULL, $wherer = NULL, $bool = NULL) {
-		if ($DB    !== NULL && (!is_file($DB) && $DB !== ":memory:")) {
-			throw new Exception($CONSTRUCTOR);
+	/*-- erros --*/
+	private $ERRORS = Array(
+		0 => "No mistakes.",
+		1 => "Database not informed.",
+		2 => "Database not found.",
+		3 => "Table not informed.",
+		4 => "The argument \$data must be a nonempty array.",
+		5 => "The argument \$where must be a nonempty array.",
+		6 => "The argument \$input must be a nonempty string.",
+	);
+
+	/*-- exibindo erros --*/
+	private function checkError($method, $code) {
+		$error = Array(
+			"error"   => $code === 0 ? False : True,
+			"message" => "{$method} - {$this->ERRORS[$code]}",
+			"id"      => 'PSWrequestError'
+		);
+		if ($code !== 0) {
+			$this->DATA = json_encode($error);
 		}
-		if ($table !== NULL && (gettype($table) !== "string" || strlen(trim($table)) == 0)) {
-			throw new Exception($TABLE);
-		}
-		if ($data  !== NULL && (gettype($data) !== "array" || count($data) == 0)) {
-			throw new Exception($DATA);
-		}
-		if ($where !== NULL && (gettype($where) !== "array" || count($where) == 0)) {
-			throw new Exception($WHERE);
-		}
-		if ($bool !== NULL && (gettype($where) !== "boolean") {
-			throw new Exception($BOOL);
+		if ($code !== 0) {
+			$method = strtolower($method);
+			throw new Exception("PSWrequestError - PSWrequest::{$method}");
 		}
 		return 0;
 	}
 
-	/*-- Construtor: define o banco de dados --*/
-	public function __construct($DB = NULL) {
-		$this->checkError($DB);
-		$this->open($DB);
-		return;
+	/*-- checando entradas --*/
+	private function checkDataBase($value) {
+		$error = 0;
+		if (gettype($value) !== "string" || strlen(trim($value)) === 0) {
+			$error = 1;
+		} else if (!is_file(trim($value)) && $value !== ":memory:")  {
+			$error = 2;
+		}
+		return $error;
+	}
+
+	private function checkTable($value) {
+		$error = 0;
+		if (gettype($value) !== "string" || strlen(trim($value)) === 0) {
+			$error = 3;
+		}
+		return $error;
+	}
+
+	private function checkData($value) {
+		$error = 0;
+		if (gettype($value) !== "array" || count($value) === 0) {
+			$error = 4;
+		}
+		return $error;
+	}
+
+	private function checkWhere($value) {
+		$error = 0;
+		if (gettype($value) !== "array" || count($value) === 0) {
+			$error = 5;
+		}
+		return $error;
+	}
+
+	private function checkSQL($value) {
+		$error = 0;
+		if (gettype($value) !== "string" || strlen(trim($value)) === 0) {
+			$error = 6;
+		}
+		return $error;
+	}
+
+	/*-- obtendo WHERE --*/
+	private function getWhere($where, $and = True) {
+		if ($this->checkWhere($where) === 0) {
+			$whr = [];
+			foreach($where as $key => $value) {
+				array_push($whr, "{$key} = '{$value}'");
+			}
+			$whr = join(($and == True ? " AND " : " OR "), $whr);
+		} else {
+			$whr = "";
+		}
+		return $whr;
 	}
 
 	/*-- output: imprime o retorno da requisição --*/
@@ -54,16 +104,33 @@ class PSWrequest extends SQLite3 {
 			$data["message"] = $this->lastErrorMsg();
 			$data["id"]      = $this->lastInsertRowID();
 		} else {
-			while ($i = $exec->fetchArray()) {
-				array_push($data, $i);
+			while ($array = $exec->fetchArray()) {//print_r($i);
+				$item = Array();
+				foreach ($array as $key => $value) {
+					if (gettype($key) === "string") {
+						$item[$key] = $value;
+					}
+				}
+				array_push($data, $item);
 			}
 		}
-		echo json_encode($data);
+		$this->DATA = json_encode($data);
 		return 0;
+	}
+
+	/*-- PÚBLICO --*/
+
+	/*-- Construtor: define o banco de dados --*/
+	public function __construct($db = ":memory:") {
+		$this->checkError("CONSTRUCTOR", $this->checkDataBase($db));
+		$this->open(trim($db));
+		return;
 	}
 
 	/*-- sql: define o que fazer a partir de um comando sql --*/
 	public function sql($input) {
+		$this->checkError("SQL", $this->checkSQL($input));
+
 		$sql   = trim($input);
 		$query = preg_match('/^SELECT/i', $sql) ? True : False;
 		$exec  = $query ? $this->query($sql) : $this->exec($sql);
@@ -73,7 +140,9 @@ class PSWrequest extends SQLite3 {
 	
 	/*-- insert: insere dados na tabela a partir de um array --*/
 	public function insert($table, $data) {
-		$this->checkError(NULL, $table, $data);
+		$this->checkError("INSERT", $this->checkTable($table));
+		$this->checkError("INSERT", $this->checkData($data));
+
 		$col = [];
 		$val = [];
 		foreach($data as $key => $value) {
@@ -89,52 +158,52 @@ class PSWrequest extends SQLite3 {
 
 	/*-- update: atualiza dados da tabela a partir de um array --*/
 	public function update($table, $data, $where, $and = True) {
-		$this->checkError(NULL, $table, $data, $where, $and);
+		$this->checkError("UPDATE", $this->checkTable($table));
+		$this->checkError("UPDATE", $this->checkData($data));
+		$this->checkError("UPDATE", $this->checkWhere($where));
+
 		$set = [];
-		$whr = [];
 		foreach($data as $key => $value) {
 			array_push($set, "{$key} = '{$value}'");
 		}
-		foreach($where as $key => $value) {
-			array_push($whr, "{$key} = '{$value}'");
-		}
 		$set = join(", ", $set);
-		$whr = join(($and == True ? " AND " : " OR "), $whr);
+		$whr = $this->getWhere($where, $and);
 		$sql = "UPDATE {$table} SET {$set} WHERE {$whr};";
 		$this->sql($sql);
 		return 0;
 	}
 
 	/*-- delete: exclui dados da tabela a partir de um array --*/
-	public function update($table, $where, $and = True) {
-		$this->checkError(NULL, $table, NULL, $where, $and);
-		$whr = [];
-		foreach($where as $key => $value) {
-			array_push($whr, "{$key} = '{$value}'");
-		}
-		$whr = join(($and == True ? " AND " : " OR "), $whr);
+	public function delete($table, $where, $and = True) {
+		$this->checkError("DELETE", $this->checkTable($table));
+		$this->checkError("DELETE", $this->checkWhere($where));
+
+		$whr = $this->getWhere($where, $and);
 		$sql = "DELETE FROM {$table} WHERE {$whr};";
 		$this->sql($sql);
 		return 0;
 	}
 
 	/*-- view: returna todos os itens da pesquisa --*/
-	public function view($table, $where, $and = True) {
-		$this->checkError(NULL, $table, NULL, NULL, $and);
-		if (gettype($where) == "array" && count($where) > 0) {
-			$whr = [];
-			foreach($where as $key => $value) {
-				array_push($whr, "{$key} = '{$value}'");
-			}
-			$whr = join(($and == True ? " AND " : " OR "), $whr);
-			$whr = " WHERE {$whr}"
-		} else {
-			$whr = "";
-		}
-		$sql = "SELECT * FROM {$table}{$whr};";
+	public function view($table, $where = Array(), $and = True) {
+		$this->checkError("VIEW", $this->checkTable($table));
+		
+		$whr = $this->getWhere($where, $and);
+		$sql = $whr !== "" ? "SELECT * FROM {$table} WHERE {$whr};" : "SELECT * FROM {$table};";
 		$this->sql($sql);
 		return 0;
 	}
-
-};
+	
+	/*--Imprime o resultado --*/
+	public function show() {
+		echo $this->DATA;
+		return 0;
+	}
+}
+$a = new PSWrequest('');
+$a->sql("create table if not exists doida (a TEXT, b NUMBER);");
+$a->insert("doida", $_GET);
+$a->insert("doida", $_GET);
+$a->view("doida");
+$a->show();
 ?>
